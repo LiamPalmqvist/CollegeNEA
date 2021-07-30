9ff9a691acfe6e2bdc1051cd9c0adb189183d21d
LiamPalmqvist <liam.palmqvist@icloud.com>
2021-07-30 21:36:28 +0100

    Adding database stuffs

---
+++ REQUIREMENTS.md
@@ -0,0 +1,18 @@
+# REQUIREMENTS
+
+### Main GUI
+
+- [ ] 
+
+### Attendance GUI
+
+- [ ] Database control for taking attendance
+- [ ] GUI for taking people’s attendance
+
+### Competition GUI
+
+- [ ] GUI for signing up people for competition
+
+### Adding new member
+
+- [ ] GUI for adding member
--- main.py
+++ main.py
@@ -4,9 +4,25 @@ from tkinter.ttk import *
 import sqlite3
 
 
-def main():
-    print("Hello")
+class MainView(Tk):
+
+    def __init__(self):
+        Tk.__init__(self)
+        window = Frame(self)
+
+        window.pack(side=TOP, fill=BOTH, expand=True)
+        window.grid_rowconfigure(0, weight=1)
+        window.grid_columnconfigure(0, weight=1)
+
+        # Defining the config within the master window
+        Tk.configure(self, bg="ivory2")
+        Tk.title(self, "Judo Databasing System")
+        Tk.option_add(self, '*Font', 'helvetica 14')
+        Tk.option_add(self, '*Background', 'ivory2')
+        Tk.geometry(self, '800x700+600+300')
+        # End of defining the config within the master window
 
 
 if __name__ == "__main__":
-    main()
+    app = MainView()
+    app.mainloop()
